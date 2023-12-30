caba64de6d7c37c56d7468a252a0bba94688c5b9
Nuhxly <68903428+Nuhxly@users.noreply.github.com>
2023-12-30 17:12:37 +0300

    fix #1

--- SkeletonReborn/Plugin.cs
+++ SkeletonReborn/Plugin.cs
@@ -14,7 +14,7 @@ namespace SkeletonReborn
        
         public static Plugin Instance;
         public override Version RequiredExiledVersion => new Version(8, 3, 9, 0);
-        public override Version Version => new Version(1, 2, 5);
+        public override Version Version => new Version(1, 2, 6);
         public override string Name => "SkeletonReborn";
         public override string Author => "Nuhxly";
         public override string Prefix => "skeleton_reborn";
@@ -44,8 +44,7 @@ namespace SkeletonReborn
         {
             
             Scps = Player.List.Where(p => p.IsScp).ToList();
-            Player randomScp = Scps[random.Next(Scps.Count)];
-
+            
             // run the coroutine after a small delay to ensure all players have spawned
             Timing.CallDelayed(0.01f, () =>
             {
@@ -53,6 +52,7 @@ namespace SkeletonReborn
                 // check if there are any SCPs on the server
                 if (Scps.Count > 0)
                 {
+                    Player randomScp = Scps[random.Next(Scps.Count)];
 
                     if (Scps.Count <= 3)
                     {
@@ -67,13 +67,11 @@ namespace SkeletonReborn
                         {
                             SkeletonScp.SkeletonScpSetter(randomScp);
                         }
-                            
                     }
                     else
                     {
                         Config.SendBroadcast(randomScp, "An error occurred and stopped you from becoming Scp-3114(Skeleton)");
                     }
-                    
                 }
             });
 
